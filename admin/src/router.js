392fab57703f32d51597ca004629185a0d3702f5
yato <2602231824@qq.com>
2020-04-19 14:31:00 +0800

    广告位处理

--- admin/src/router.js
+++ admin/src/router.js
@@ -13,6 +13,12 @@ import HeroList from './views/HeroList.vue'
 import ArticleEdit from './views/ArticleEdit.vue'
 import ArticleList from './views/ArticleList.vue'
 
+import AdEdit from './views/AdEdit.vue'
+import AdList from './views/AdList.vue'
+
+import AdminUserEdit from './views/AdminUserEdit.vue'
+import AdminUserList from './views/AdminUserList.vue'
+
 Vue.use(Router)
 
 export default new Router({
@@ -36,6 +42,15 @@ export default new Router({
                 { path: '/articles/create', component: ArticleEdit },
                 { path: '/articles/list', component: ArticleList },
                 { path: '/articles/edit/:id', component: ArticleEdit, props: true },
+
+
+                { path: '/ads/create', component: AdEdit },
+                { path: '/ads/list', component: AdList },
+                { path: '/ads/edit/:id', component: AdEdit, props: true },
+
+                { path: '/admin_users/create', component: AdminUserEdit },
+                { path: '/admin_users/list', component: AdminUserList },
+                { path: '/admin_users/edit/:id', component: AdminUserEdit, props: true },
             ]
         },
         {
---
+++ admin/src/views/AdEdit.vue
@@ -0,0 +1,77 @@
+  <template>
+  <div>
+    <h1>{{ id ? "编辑" : "新建" }}广告位</h1>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="名称">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="广告">
+        <el-button @click="model.items.push({})">
+          <i class="el-icon-plus"></i> 添加广告
+          </el-button>
+        <el-row>
+          <el-col :md="24" v-for="(item,idx) in model.items" :key=idx>
+            <el-form-item label="跳转链接(URL)">
+              <el-input v-model="item.url "></el-input>
+            </el-form-item>
+            <el-form-item label="图标" style='margin-top: 1rem'>
+              <el-upload
+                class="avatar-uploader"
+                :action="$http.defaults.baseURL + '/upload'"
+                :show-file-list="false"
+                :on-success="res=> $set(item, 'image', res.url)"
+              >
+                <img v-if="item.image" :src="item.image" class="avatar" />
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="danger" 
+              @click="model.items.splice(idx, 1)">删除</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+/* eslint-disable */
+export default {
+  props: {
+    id: {}
+  },
+  data() {
+    return {
+      model: {
+        items: []
+      }
+    };
+  },
+  methods: {
+    async save() {
+      let res;  
+      if (this.id) {
+        res = await this.$http.put(`rest/ads/${this.id}`, this.model);
+      } else {
+        res = await this.$http.post("rest/ads", this.model);
+      }
+      this.$router.push("/ads/list");
+      this.$message({
+        type: "success",
+        message: res.data
+      });
+    },
+    async fecth() {
+      const res = await this.$http.get(`rest/ads/${this.id}`);
+      this.model = Object.assign({}, this.model, res.data);
+    }
+  },
+  created() {
+    this.id && this.fecth();
+  }
+};
+</script>
